92bc157d86c815da51a1120b67469c7af9e11b1a
RajatSaini123 <73208617+RajatSaini123@users.noreply.github.com>
2021-10-23 22:20:16 -0700

    Add files via upload

---
+++ c104/mean.py
@@ -0,0 +1,19 @@
+import csv 
+
+with open ('HeightWeight.csv',newline='')as f:
+    reader=csv.reader(f)
+    filedata=list(reader)
+filedata.pop(0)
+
+newdata=[]
+for i in range(len(filedata)):
+    n_num=filedata[i][2]
+    newdata.append(float(n_num))
+
+n=len(newdata)
+total=0
+for x in newdata:
+    total+=x
+
+mean=total/n
+print("mean: "+str(mean))
